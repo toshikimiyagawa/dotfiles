8931569e44f425fd10fff154bb6d2f57b0a95823
Toshiki Miyagawa <toshi402@gmail.com>
2017-04-09 12:20:22 +0000

    プロンプトのカレントディレクトリの表示を変更

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,10 @@
 export LANG=ja_JP.UTF8
+set -U fish_prompt_pwd_dir_length 0
 
 function fish_prompt
   set status_face (set_color green)"\$ "
 
-  set prompt (set_color yellow)"[ "(prompt_pwd)"/ ]"
+  set prompt (set_color yellow)"[ "(pwd)"/ ]"
 
   echo $prompt
   echo $status_face

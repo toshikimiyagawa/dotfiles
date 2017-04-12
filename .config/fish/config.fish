84b41b1d7e8a8216c58ab1f039e7b1b810faf01f
Toshiki Miyagawa <toshi402@gmail.com>
2017-04-12 23:43:14 +0000

    プロンプトのカレントディレクトリのレイアウトを変更

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,8 +4,12 @@ set -U fish_prompt_pwd_dir_length 0
 function fish_prompt
   set status_face (set_color green)"\$ "
 
-  set prompt (set_color yellow)"[ "(pwd)"/ ]"
+  set prompt (set_color yellow)"["(pwd)"/]"
 
   echo $prompt
   echo $status_face
+end
+
+function fish_user_key_bindings
+  bind \cr peco_select_history
 end

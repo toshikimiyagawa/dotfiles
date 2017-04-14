57a0204eabe93b383f9ea577cab3fbe15a79b226
Toshiki Miyagawa <toshi402@gmail.com>
2017-04-14 15:49:52 +0000

    プロンプト修正

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,11 @@
-export LANG=ja_JP.UTF8
+set -x LANG ja_JP.UTF8
+set -x GOPATH $HOME/go/
 set -U fish_prompt_pwd_dir_length 0
 
 function fish_prompt
   set status_face (set_color green)"\$ "
 
-  set prompt (set_color yellow)"[ "(pwd)"/ ]"
+  set prompt (set_color yellow)"["(pwd)"/]"
 
   echo $prompt
   echo $status_face

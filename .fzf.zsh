6810232c3d4ac623880841980895d32da3447126
Toshiki Miyagawa <toshi402@gmail.com>
2024-02-12 20:42:56 +0900

    update fzf directory

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,16 +1,16 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
-fi
+#if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
+#  PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
+#fi
 
 # Auto-completion
 # ---------------
-source "/opt/homebrew/opt/fzf/shell/completion.zsh"
+source "~/.config/fzf/completion.zsh"
 
 # Key bindings
 # ------------
-source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
+source "~/.config/fzf/key-bindings.zsh"
 
 function ghq-fzf() {
   local target_dir=$(ghq list -p | fzf --query="$LBUFFER")
